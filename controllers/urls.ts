34d02bf7164cdfa8ed8b53c19e4e88ce80624ed7
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-02-20 10:57:23 +0100

    POST logic and GET all URLs

--- controllers/urls.ts
+++ controllers/urls.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
+import { validateUrl } from '../helpers/validation';
+import Url from '../models/url';
+import { RequestUser } from '../types/picotypes';
 
 async function getAllUrls(req: Request, res: Response) {
-    return res.status(200).send("GET success");
+    try {
+        const urls = await Url.find({ userId: (req as Request & RequestUser).user._id });
+        return res.status(200).json(urls);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Unable to retrieve URLs');
+    }
 }
 
 async function getUrl(req: Request, res: Response) {
@@ -13,7 +22,27 @@ async function redirectUrl(req: Request, res: Response) {
 }
 
 async function createUrl(req: Request, res: Response) {
-    return res.status(200).send("POST success");
+    const { error } = validateUrl(req.body);
+    if (error) {
+        console.log(error);
+        return res.status(400).send(error.details[0].message);
+    }
+    let url = await Url.findOne({ originalUrl: req.body.url });
+    if (url) {
+        return res.status(200).json({ shortUrl: url.shortUrl });
+    }
+    url = new Url({
+        userId: (req as Request & RequestUser).user._id,
+        originalUrl: req.body.url,
+        shortUrl: "aBcDE01234",
+    });
+    try {
+        await url.save();
+        return res.status(201).json(url);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Error shortening URL');
+    }
 }
 
 async function updateUrl(req: Request, res: Response) {
